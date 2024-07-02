1f514823825936f6b63e91fafa3a15859ed1c8c9
Rogers F. Silva <rsilva@gsu.edu>
2024-07-02 20:06:51 +0000

    Updated the file paths

--- slurm/misa_mathpath.sh
+++ slurm/misa_mathpath.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH -e /data/users1/cmccurdy5/MISA-pytorch/slurm_logs/error%A-%a.err
-#SBATCH -o /data/users1/cmccurdy5/MISA-pytorch/slurm_logs/out%A-%a.out
+#SBATCH -e /data/users3/rsilva/MISA-pytorch/slurm_logs/error%A-%a.err
+#SBATCH -o /data/users3/rsilva/MISA-pytorch/slurm_logs/out%A-%a.out
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=cmccurdy5@student.gsu.edu
-#SBATCH --chdir=/data/users1/cmccurdy5/MISA-pytorch
+#SBATCH --mail-user=rsilva@student.gsu.edu
+#SBATCH --chdir=/data/users3/rsilva/MISA-pytorch
 #
 #SBATCH -p qTRDGPU
 #SBATCH --gres=gpu:A40:1
@@ -44,7 +44,8 @@ beta1=(0.7)
 beta2=(0.65)
 
 experimenter="$USER"
-configuration="/data/users1/cmccurdy5/MISA-pytorch/configs/sim-siva.yaml"
+configuration="/data/users3/rsilva/MISA-pytorch/configs/sim-siva.yaml"
+
 data_file="sim-siva_dataset"$n_dataset"_source"$n_source"_sample"$n_sample"_seed"$SEED".mat"
 declare -i num_experiments=${#lrs[@]}
 for ((i=0; i<num_experiments; i++)); do
